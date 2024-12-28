a52ea4154e4b07cea44c9f549c41a4544d5ad681
David Irwin <dirwin-solutions@universallynull.com>
2024-12-28 17:44:36 -0500

    Recreate index.js as index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 const express = require('express')
 const PORT = 4971
-app = express()
-
-app.get('/', async (req, res) => {
-  res.send('hi')
-})
-
+const app = express()
 app.listen(PORT)
 console.log(`App active on http://localhost:${PORT}`)
