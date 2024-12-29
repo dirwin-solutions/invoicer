a530498504e44c159670a0359b5607cf2b44d9bb
David Irwin <dirwin-solutions@universallynull.com>
2024-12-28 22:47:03 -0500

    Standardize package element implementations

--- src/client/model.ts
+++ src/client/model.ts
@@ -2,10 +2,8 @@ import Model from '../base/Model'
 import { Pool } from 'pg'
 import { Client } from './types'
 
-class ClientModel extends Model<Client> {
+export default class ClientModel extends Model<Client> {
   constructor(pool: Pool) {
     super(pool, 'client')
   }
 }
-
-export default ClientModel
--- src/client/service.ts
+++ src/client/service.ts
@@ -2,10 +2,8 @@ import Service from '../base/Service'
 import ClientModel from './model'
 import { Client } from './types'
 
-class ClientService extends Service<Client> {
+export default class ClientService extends Service<Client> {
   constructor(model: ClientModel) {
     super(model)
   }
 }
-
-export default ClientService
