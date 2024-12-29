c7e401718bddea48d2aa5c60567eeb3a8aef7e32
David Irwin <dirwin-solutions@universallynull.com>
2024-12-28 22:54:32 -0500

    Expose list at Controller layer

--- src/base/Controller.ts
+++ src/base/Controller.ts
@@ -6,12 +6,22 @@ export default class Controller<T> {
   constructor(service: Service<T>) {
     this.service = service
     this.getById = this.getById.bind(this)
+    this.list = this.list.bind(this)
   }
 
   async getById(req: Request, res: Response, next: NextFunction) {
     try {
       const id = Number(req.params.id)
       const result = await this.service.getById(id)
+      res.json(result)
+    } catch(e) {
+      next(e)
+    }
+  }
+
+  async list(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await this.service.list()
       res.json(result)
     } catch(e) {
       next(e)
