d87da5f7ecba7c2673c7f98dcefbbfbab512f59a
David Irwin <dirwin-solutions@universallynull.com>
2024-12-28 21:05:20 -0500

    Create base Router and Controller classes

---
+++ src/base/Controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express'
+import Service from './Service'
+
+export default class Controller<T> {
+  protected service: Service<T>
+  constructor(service: Service<T>) {
+    this.service = service
+  }
+
+  async getById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = Number(req.params.id)
+      const result = await this.service.getById(id)
+      res.json(result)
+    } catch(e) {
+      next(e)
+    }
+  }
+}
+
