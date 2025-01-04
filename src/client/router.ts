3856f93b95535c9191a2314027b03577370ecc3a
David Irwin <dirwin-solutions@universallynull.com>
2025-01-04 14:18:02 -0500

    Remove create and updateById

--- src/client/router.ts
+++ src/client/router.ts
@@ -5,7 +5,5 @@ import { Client } from './types'
 export default class ClientRouter extends RouterBase<Client> {
   constructor(controller: ClientController) {
     super(controller)
-    this.router.post('/', controller.createClient)
-    this.router.patch('/:id', controller.updateClientById)
   }
 }
