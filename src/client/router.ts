8375b4bac6183c7186db1c5cb57eaa620c979dfb
David Irwin <dirwin-solutions@universallynull.com>
2025-01-04 15:27:20 -0500

    Remove unnecessary files

--- src/client/router.ts
+++
@@ -1,9 +0,0 @@
-import RouterBase from '../base/Router'
-import ClientController from './controller'
-import { Client } from './types'
-
-export default class ClientRouter extends RouterBase<Client> {
-  constructor(controller: ClientController) {
-    super(controller)
-  }
-}
