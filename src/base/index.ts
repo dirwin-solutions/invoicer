c9dc01e1a000b27b1b84c2ef82107749959e187b
David Irwin <dirwin-solutions@universallynull.com>
2025-01-04 15:24:21 -0500

    Expose types at base index

--- src/base/index.ts
+++ src/base/index.ts
@@ -2,10 +2,14 @@ import Model from './Model'
 import Service from './Service'
 import Controller from './Controller'
 import ApiRouter from './ApiRouter'
+import { DatabaseEntity, CreateInput, UpdateInput } from './types'
 
 export {
   Model,
   Service,
   Controller,
-  ApiRouter
+  ApiRouter,
+  DatabaseEntity,
+  CreateInput,
+  UpdateInput
 }
