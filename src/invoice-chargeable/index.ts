573a0545948a4a6545939d8c83ebce13dbc99e52
David Irwin <dirwin-solutions@universallynull.com>
2025-02-02 18:15:48 -0500

    Expose new InvoiceChargeable Model and Service

--- src/invoice-chargeable/index.ts
+++ src/invoice-chargeable/index.ts
@@ -1,6 +1,10 @@
 import { InvoiceChargeable, InvoiceChargeableDetail } from './types'
+import InvoiceChargeableModel from './model'
+import InvoiceChargeableService from './service'
 
 export {
   InvoiceChargeable,
   InvoiceChargeableDetail,
+  InvoiceChargeableModel,
+  InvoiceChargeableService,
 }
