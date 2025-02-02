d02e6845143cdd16b31e9a6332846e595b1f93ee
David Irwin <dirwin-solutions@universallynull.com>
2025-02-02 17:54:42 -0500

    Create Detail type

--- src/base/types.ts
+++ src/base/types.ts
@@ -4,4 +4,5 @@ export interface DatabaseEntity {
   updatedAt?: Date
 }
 export type CreateInput<T> = Omit<T, 'id' | 'updatedAt' | 'createdAt'>
+export type Detail<T> = Omit<T, 'createdAt' | 'updatedAt'>
 export type UpdateInput<T> = Partial<CreateInput<T>> & { updatedAt?: Date }
